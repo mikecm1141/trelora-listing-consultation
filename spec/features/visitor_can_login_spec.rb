3ad952096b2e23b039313e3c9e9cb14a0f30fe8b
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-10-24 16:12:01 -0600

    Add all addresses to test. Test passing.

--- spec/features/visitor_can_login_spec.rb
+++ spec/features/visitor_can_login_spec.rb
@@ -11,7 +11,8 @@ describe 'As a visitor' do
 
             expect(current_path).to eq("/find")
             expect(page).to have_content("Pricing Location Address")
-            expect(page).to have_content("")
+            expect(page).to have_content("1860_south_marion_street-Denver-CO-80210")
+            expect(page).to have_content("910-portland_place-Boulder-CO-80304")
         end
     end
 end
