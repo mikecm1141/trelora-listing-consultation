89d5d89710c2d2217a9741bdae69f16948581142
Harper Bellows <harper.bellows@gmail.com>
2018-10-27 13:58:03 -0600

    remove unused model tests generated by rails

--- spec/models/user_property_spec.rb
+++ spec/models/user_property_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe UserProperty, type: :model do
-  describe 'Relationships' do
-    it { should have_one(:property_id).class_name("Property").with_foreign_key("listing_id") }
-  end
+
 end
