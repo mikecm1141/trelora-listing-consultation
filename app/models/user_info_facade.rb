48aa5ca01a8d0d0c8cbb9828516569d48dbbdb3b
mmbensalah <mmbensalah@gmail.com>
2018-10-27 08:51:39 -0600

    makes changes to user info facade

--- app/models/user_info_facade.rb
+++ app/models/user_info_facade.rb
@@ -5,23 +5,14 @@ class UserInfoFacade
   end
 
   def get_addresses
-    # response = trelora_service.get_user_info(@email, @password)
-
     addresses_hash = get_user_info[:data][:available_address][:addresses]
-
     addresses = addresses_hash.map do |key, value|
         value[:id]
     end
   end
 
-
   def get_token
-    # response = trelora_service.get_user_info(@email, @password)
-
     get_user_info[:data][:user][:HTTP_AUTH_TOKEN]
-  end
-
-  def get_user_info
   end
 
   private
