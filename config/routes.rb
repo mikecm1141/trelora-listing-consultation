d526dfbdb0a64bfb16e720779ba7dc3e6e59f205
Harper Bellows <35637783+hbellows@users.noreply.github.com>
2018-10-27 15:56:18 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "login_users#new"
+  root to: 'login_users#new'
   get '/find-property', to: 'find#index'
   resources :login_users, only: [:index, :create, :new]
 
