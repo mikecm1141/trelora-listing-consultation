92552ec61fd2353f7bd93b24bf7fdd3c2313b575
Mike <mikecm@gmail.com>
2018-10-28 10:08:48 -0600

    Add DB migration for user property table for string addresses

--- app/controllers/find_controller.rb
+++ app/controllers/find_controller.rb
@@ -1,5 +1,16 @@
 class FindController < ApplicationController
-  def index
-    @addresses = session[:addresses]
+  def index; end
+
+  def create
+  	clean_address = params[:address].gsub(/[[:punct:]]/, '').downcase
+  	find_property = UserProperty.find_by_address(clean_address)
+
+  	if find_property.nil?
+  		flash[:alert] = "* Address Not Found"
+  		render :index
+  	end
+
+  	session[:property_found] = find_property.listing_id
+  	redirect_to '/prepare'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'login_users#new'
-  get '/find-property', to: 'find#index'
   resources :login_users, only: %w[index create new]
+
+  get '/find-property', to: 'find#index'
+  post '/find-proprety', to: 'find#create'
 end
