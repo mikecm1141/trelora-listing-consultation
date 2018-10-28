4365ae2ddab5cd64396d329d101c930d678b84fb
Mike <mikecm@gmail.com>
2018-10-28 11:10:21 -0600

    fixes top links to disable depending on app state

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def active(path)
     'active' if current_page?(path)
   end
+
+  def link_to_path(path)
+    "href=#{path}" if session[:property_found]
+  end
+
+  def disabled_class(path)
+    'is-disabled' unless link_to_path(path)
+  end
 end
